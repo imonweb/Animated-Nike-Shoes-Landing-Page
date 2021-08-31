e59ba06ef3690a14b9d98e508ac4692c60860528
imonweb <contact@imonweb.co.uk>
2021-08-31 23:09:19 +0100

    completed

--- app.js
+++ app.js
@@ -1,70 +1,91 @@
 let slideIndex = 0;
 
-let slider = document.getElementById('slider');
+let slider = document.getElementById('slider')
 
-let slides = slider.getElementsByClassName('slide');
+let slides = slider.getElementsByClassName('slide')
 
-let slideControl = document.getElementById('slide-control');
+let slideControl = document.getElementById('slide-control')
 
-let slideControlItems = slideControl.getElementsByClassName('slide-control-item');
+let slideControlItems = slideControl.getElementsByClassName('slide-control-item')
 
-slider.style.marginTop = '-' + slideIndex + '00vh';
+
+slider.style.marginTop = '-' + slideIndex + '00vh'
 
 setTimeout(() => {
-  slideControlItems[slideIndex].classList.add('active');
-  slides[slideIndex].classList.add('active');
+	slideControlItems[slideIndex].classList.add('active')
+	slides[slideIndex].classList.add('active')
 }, 500)
 
 
 chooseProduct = (index) => {
-  if(index === slideIndex) return
+	if (index === slideIndex) return
 
-  slideIndex = index
+	slideIndex = index
 
-  let currSlideControl = slideControl.querySelector('.slide-control-item.active')
-  currSlideControl.classList.remove('active')
+	let currSlideControl = slideControl.querySelector('.slide-control-item.active')
+	currSlideControl.classList.remove('active')
 
-  let currSlide = slider.querySelector('.slide.active')
-  
-  setTimeout(() => {
-    slider.style.marginTop = '-' + slideIndex + '00vh';
-    slideControlItems[slideIndex].classList.add('active');
-    slides[slideIndex].classList.add('active');
-  }, 1500)
+	let currSlide = slider.querySelector('.slide.active')
+	currSlide.classList.remove('active')
+
+	setTimeout(() => {
+		slider.style.marginTop = '-' + slideIndex + '00vh'
+		slideControlItems[slideIndex].classList.add('active')
+		slides[slideIndex].classList.add('active')
+	}, 1500)
+	
 }
 
 Array.from(slideControlItems).forEach((el, index) => {
-  el.onclick = function() {
-    chooseProduct(index)
-  }
+	el.onclick = function() {
+		chooseProduct(index)
+	}
 })
 
-let modal = document.getElementById('modal');
+let modal = document.getElementById('modal')
 
-let closeBtn = document.getElementById('modal-close');
+let closeBtn = document.getElementById('modal-close')
 
 closeBtn.onclick = () => {
-  modal.style.display = 'none'
+	modal.style.display = 'none'
 }
 
 let moreImages = document.getElementsByClassName('more-images-item')
+
 let previewImages = document.getElementsByClassName('img-preview')
 
 Array.from(moreImages).forEach((el) => {
-  el.onclick = () => {
-    let imgItems = el.parentNode.geetElementsByTagName('img')
+	el.onclick = () => {
+		let imgItems = el.parentNode.getElementsByTagName('img')
 
-    Array.from(imgItems).forEach((item, index) => {
-      previewImages[index].src = item.src;
-    })
+		Array.from(imgItems).forEach((item, index) => {
+			previewImages[index].src = item.src
+		})
 
-    let img = el.querySelector('img')
-    modal.style.display = 'block'
+		let img = el.querySelector('img')
+		modal.style.display = 'block'
 
-    let modalContent = modal.querySelector('.modal-content')
-    modalContent.src = img.src 
+		let modalContent = modal.querySelector('.modal-content')
+		modalContent.src = img.src
 
-    let temp = modalContent.closeNode(true)
-    modalContent.parentNode.replaceChild(temp, modalContent)
-  }
+		let temp = modalContent.cloneNode(true)
+		modalContent.parentNode.replaceChild(temp, modalContent)
+	}
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
